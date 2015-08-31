79f0eec2ed48b29372cdee723c0b430798d0ff86
ikonglong <konglong.me@gmail.com>
2015-08-31 18:03:51 +0800

    remove unnecessary imports

--- liurushi-community/src/main/java/com/beautysight/liurushi/community/app/WorkApp.java
+++ liurushi-community/src/main/java/com/beautysight/liurushi/community/app/WorkApp.java
@@ -37,7 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.jvm.hotspot.jdi.IntegerValueImpl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
