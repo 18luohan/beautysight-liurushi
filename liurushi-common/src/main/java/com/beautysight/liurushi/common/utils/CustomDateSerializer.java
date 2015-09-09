44ba8b1a250615d2bdaccf0c57f05c3df05bb790
ikonglong <konglong.me@gmail.com>
2015-09-09 16:43:52 +0800

    change date as UTC time

--- liurushi-common/src/main/java/com/beautysight/liurushi/common/utils/CustomDateSerializer.java
+++ liurushi-common/src/main/java/com/beautysight/liurushi/common/utils/CustomDateSerializer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
@@ -28,8 +29,8 @@ public class CustomDateSerializer extends JsonSerializer<Date> {
 
     @Override
     public void serialize(Date value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-        // TODO 时间转换时，请把时区也考虑进来
-        gen.writeString(fmt.print(new DateTime(value)));
+        // 统一转换为UTC时间
+        gen.writeString(fmt.print(new DateTime(value, DateTimeZone.UTC)));
     }
 
 }
